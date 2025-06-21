8f8ef569d7c25f22f70108ea3333906ff2ff023e
GooseyEvan <56296788+p17166907@users.noreply.github.com>
2025-06-21 19:34:14 +0100

    product-card, product-price

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
-import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
-import { ThemeProvider } from "next-themes";
+import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 // Metadata interface to describe all the metadata fields that can be set in a document.
 export const metadata: Metadata = {
-  // we are using a template to set the title. So if we set a title for an individual page, it will be prepended with the APP_NAME and a pipe character.
-  title: { template: `%s| ${APP_NAME}`, default: APP_NAME },
-  description: APP_DESCRIPTION,
-  //metadataBase is used to set the base URL for the metadata.
-  metadataBase: new URL(SERVER_URL),
+  title: "Prostore",
+  description: "A modern store built with Next.js",
 };
 
 export default function RootLayout({
@@ -24,14 +21,11 @@ export default function RootLayout({
     // React hydration warning due to a mismatch between server-rendered and client-rendered HTML fix = suppressHydrationWarning.
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`} suppressHydrationWarning>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <div className="flex h-screen flex-col">
+          <Header />
+          <main className="flex-1 wrapper">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,42 @@
+import ProductCard from "./product-card";
+
+export type Product = {
+  name: string;
+  slug: string;
+  brand: string;
+  price: number;
+  rating: number;
+  stock: number;
+  images?: string[];
+};
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: Product[];
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4">{title}</h2>
+
+      {limitedData.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {limitedData.map((product) => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
